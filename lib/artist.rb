203496951760cbb3816af7491c172be2ae1325d4
Zaire  Swailes <swaileszaire@gmail.com>
2020-04-06 06:20:20 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist 
+  #if the artist exists in the program 
+  
+  #if the artist doesnt exist in the program yet
+  
+  attr_accesor :name, :song 
+  
+  def initialize(name)
+    @name = name 
+    @song = self 
+  end 
+  
+  @@all << initialize(name)
+  
+  def self.all 
+    return initialize
+  end 
+  
+  
